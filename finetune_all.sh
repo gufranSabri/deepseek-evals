358053dcdae1b6262f869adc0fed2a9ceae6579a
Ahmed Hasanaath <61200955+gufranSabri@users.noreply.github.com>
2025-01-31 22:49:01 +0300

    changed bash file

--- finetune_all.sh
+++ finetune_all.sh
@@ -1,9 +1,13 @@
+#taqyim datasets - ordered on basis of size
+
 python finetune.py --task sentiment
-python finetune.py --task diacratization
-# python finetune.py --task mcq
 python finetune.py --task pos_tagging
-python finetune.py --task summarization
-python finetune.py --task translation
-python finetune.py --task transliteration
 python finetune.py --task paraphrasing
+python finetune.py --task summarization
+python finetune.py --task transliteration
+python finetune.py --task diacratization
+# python finetune.py --task translation
+
+# =================================
+# python finetune.py --task mcq
 # python finetune.py --task GQA
