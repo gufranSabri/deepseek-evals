c0d6577e1afb1c22ddc5a7c16ca48abbc6ba9ba2
Ahmed-Ashraf-Marzouk <a.a.elghawas@gmail.com>
2025-04-15 14:43:01 +0300

    cleaning code for resusability

--- zs_inference.py
+++ zs_inference.py
@@ -23,23 +23,45 @@ class ZS_Inference:
         self.shots = args.shots
         self.save_path = args.save_path
         self.call_limit = args.call_limit
+        self.stop_point = args.stop_point
+        self.base_url = args.base_url
+        self.client = args.client
+        self.api_key = args.api_key
+        self.request_minute = args.request_minute
+        self.max_tokens = args.max_tokens
+        self.temperature = args.temperature
+        self.top_k = args.top_k
+        self.top_p = args.top_p
+        self.stream = args.stream
+        self.repetition_penalty = args.repetition_penalty
+        self.stop_word = args.stop_word
 
         self.API_MODELS = {
             # "V3": "deepseek-ai/DeepSeek-V3", #nebius
             "V3": "deepseek/deepseek_v3", #novita
-            # "R1": "deepseek-ai/DeepSeek-R1",
+            # "R1": "deepseek-ai/DeepSeek-R1", #novita
             "R1": "deepseek-reasoner",
             "Q1.5B": "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
-            # "Q14B": "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B" #together
+            # "Q14B": "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B", #together
             "Q14B": "deepseek/deepseek-r1-distill-qwen-14b" #novita
+            "GPT3.5-Turbo": "gpt-3.5-turbo",
+            "GPT4": "gpt-4",
+            "O1": "o1",
+            "O1-Pro": "o1-pro",
+            "O3-Mini": "o3_mini",
+            "QWEN7B": "qwen-7b",
+            "QWEN14B": "qwen-14b"
         }
+        self.reasoners = ["R1", "o1", "o1-pro", "o3-mini"]
         self.shuffle = self.model_name in ["Q14B", "Q1.5B"]
         self.split = "test" if self.model_name in ["V3", "R1"] else "train"
 
         self.TOGETHER_MODELS = ["Q1.5B"]
-        self.NOVITA_MODELS = ["V3", "Q14B", "R1"]
+        self.NOVITA_MODELS = ["V3", "Q14B"]
         self.DEEPSEEK_MODELS = ["R1"]
-        self.NEBIUS_MODELS = []
+        self.CHATGPT_MODELS = ["gpt-3.5-turbo", "gpt-4", "gpt-4-turbo", "o1", "o1-pro", "o3-mini"]
+        self.QWEN_MODELS = ["qwen-1.8b", "qwen-7b", "qwen-14b", "qwen-72b", "qwen-plus"]
+        # self.NEBIUS_MODELS = []
 
         self.load_model()
         self.load_data()
@@ -75,20 +97,28 @@ class ZS_Inference:
         if self.local:
             self.local_model_inference()
         else:
-            if self.model_name in self.TOGETHER_MODELS:
-                self.api_model_inference_together()
-            # elif self.model_name in self.NEBIUS_MODELS:
-            #     self.api_model_inference_nebius()
-            elif self.model_name in self.NOVITA_MODELS:
-                self.api_model_inference_novita()
-            elif self.model_name in self.DEEPSEEK_MODELS:
-                self.api_model_inference_deepseek()
+            self.api_model_inference()
 
-    def api_model_inference_deepseek(self):
-        print("CALLING DEEPSEEK API")
+    
+
+    def api_model_inference(self):
+        print("CALLING " + self.model.upper() + " API")
+
+        if self.client == "together":
+            client = Together()
+        else:
+            client = OpenAI(
+                base_url=os.environ.get(self.base_url),
+                api_key=os.environ.get(self.api_key),
+            )
+
+            
+        start_time = time.time()
+        requests_sent = 0  # Track requests per minute
 
         for i, text in enumerate(self.dataset["text"]):
             if i == self.call_limit: break
+            if i <= self.stop_point: continue
 
             q, a, = None, None
             if self.prompt_lang == "ar":
@@ -98,11 +128,12 @@ class ZS_Inference:
                 q = text.split("### Response:")[0]
                 a = text.split("### Response:")[1]
 
-            client = OpenAI(
-                base_url="https://api.deepseek.com",
-                api_key="sk-1023ce7c74fa4c5aaadd299c2758f0e3",
-            ) 
-
+            if requests_sent >= self.request_minute:
+                elapsed_time = time.time() - start_time
+                if elapsed_time < 60 and self.request_minute > 0:
+                    time.sleep(60 - elapsed_time)  # Wait for the remaining time
+                start_time = time.time()  # Reset start time
+                requests_sent = 0  # Reset counter
 
             chat_completion_res = client.chat.completions.create(
                 model=self.model,
@@ -112,11 +143,14 @@ class ZS_Inference:
                         "content": f"""{q}""",
                     }
                 ],
-                # max_tokens=1024,
+                max_completion_tokens= self.max_tokens,
+                temperature=self.temperature,
+                top_p=self.top_p,
+                # stop=[self.stop_word],
+                stream=self.stream
             )
 
             res = chat_completion_res.choices[0].message.content
-            res_r = chat_completion_res.choices[0].message.reasoning_content
 
             logger = Logger(os.path.join(self.preds_file_path, f"{i}.txt"))
                 
@@ -125,9 +159,13 @@ class ZS_Inference:
             logger(a)
             logger("=================================================================================")
             logger(res)
-            logger("=================================================================================")
-            logger(res_r)
+            if self.model_name in self.reasoners:
+                logger("=================================================================================")
+                res_r = chat_completion_res.choices[0].message.reasoning_content
+                logger(res_r)
             print(f"{i} ------------------------------------------\n\n\n")
+            requests_sent += 1  # Increment request count
+            time.sleep(1.2)  # Ensure ~50 requests per minute
 
     def local_model_inference(self):
         for i, prompt in enumerate(self.dataset["text"]):
@@ -147,173 +185,6 @@ class ZS_Inference:
             else:
                 logger(response[0].split("### Response:")[1].replace("<｜end▁of▁sentence｜>", "").replace("<｜end▁of▁sentence｜>", ""))
 
-            
-    # def api_model_inference_nebius(self):
-    #     print("CALLING NEBIUS API")
-
-    #     for i, text in enumerate(self.dataset["text"]):
-    #         if i == self.call_limit: break
-
-    #         client = OpenAI(
-    #             base_url="https://api.studio.nebius.ai/v1/",
-    #             api_key=os.environ.get("OPENAI_API_KEY"),
-    #         )
-
-    #         completion = client.chat.completions.create(
-    #             model=self.model,
-    #             messages=[
-    #                 {
-    #                     "role": "user",
-    #                     "content": f"""{text}"""
-    #                 }
-    #             ],
-    #             temperature=0
-    #         )
-
-    #         j = completion.to_dict()
-    #         res = j["choices"][0]["message"]["content"]
-    #         logger = Logger(os.path.join(self.preds_file_path, f"{i}.txt"))
-    #         logger(res)
-    #         print(f"{i} ------------------------------------------\n")
-
-    def api_model_inference_deepseek(self):
-        print("CALLING DEEPSEEK API")
-
-        for i, text in enumerate(self.dataset["text"]):
-            if i == self.call_limit: break
-
-            q, a, = None, None
-            if self.prompt_lang == "ar":
-                q = text.split(":إجابة###")[0]
-                a = text.split(":إجابة###")[1]
-            else:
-                q = text.split("### Response:")[0]
-                a = text.split("### Response:")[1]
-
-            client = OpenAI(
-                base_url="https://api.deepseek.com",
-                api_key="sk-1023ce7c74fa4c5aaadd299c2758f0e3",
-            ) 
-
-
-            chat_completion_res = client.chat.completions.create(
-                model=self.model,
-                messages=[
-                    {
-                        "role": "user",
-                        "content": f"""{q}""",
-                    }
-                ],
-                # max_tokens=1024,
-            )
-
-            res = chat_completion_res.choices[0].message.content
-            res_r = chat_completion_res.choices[0].message.reasoning_content
-
-            logger = Logger(os.path.join(self.preds_file_path, f"{i}.txt"))
-                
-            logger(q)
-            logger("=================================================================================")
-            logger(a)
-            logger("=================================================================================")
-            logger(res)
-            logger("=================================================================================")
-            logger(res_r)
-            print(f"{i} ------------------------------------------\n\n\n")
-     
-    def api_model_inference_novita(self):
-        print("CALLING NOVITA API")
-
-        for i, text in enumerate(self.dataset["text"]):
-            if i == self.call_limit: break
-
-            q, a, = None, None
-            if self.prompt_lang == "ar":
-                q = text.split(":إجابة###")[0]
-                a = text.split(":إجابة###")[1]
-            else:
-                q = text.split("### Response:")[0]
-                a = text.split("### Response:")[1]
-
-            client = OpenAI(
-                base_url="https://api.novita.ai/v3/openai",
-                api_key="sk_XTZ7jxWmpXeqCiVG-QuFN3jj1FiDLK1EyOVudLleglk",
-            ) 
-
-
-            chat_completion_res = client.chat.completions.create(
-                model=self.model,
-                messages=[
-                    {
-                        "role": "user",
-                        "content": f"""{q}""",
-                    }
-                ],
-                # max_tokens=1024,
-            )
-
-            res = chat_completion_res.choices[0].message.content
-            logger = Logger(os.path.join(self.preds_file_path, f"{i}.txt"))
-                
-            logger(q)
-            logger("=================================================================================")
-            logger(a)
-            logger("=================================================================================")
-            logger(res)
-
-            print(f"{i} ------------------------------------------\n\n\n")
-    
-    def api_model_inference_together(self):
-        print("CALLING TOGETHER API")
-
-        client = Together()
-        start_time = time.time()
-        requests_sent = 0  # Track requests per minute
-
-        for i, text in enumerate(self.dataset["text"]):
-            if i == self.call_limit: break
-
-            q, a = None, None
-            if self.prompt_lang == "ar":
-                q = text.split(":إجابة###")[0]
-                a = text.split(":إجابة###")[1]
-            else:
-                q = text.split("### Response:")[0]
-                a = text.split("### Response:")[1]
-
-            if requests_sent >= 50:
-                elapsed_time = time.time() - start_time
-                if elapsed_time < 60:
-                    time.sleep(60 - elapsed_time)  # Wait for the remaining time
-                start_time = time.time()  # Reset start time
-                requests_sent = 0  # Reset counter
-
-            response = client.chat.completions.create(
-                model=self.model,
-                messages=[{"role": "user", "content": f"""{q}"""}],
-                # max_tokens=1024,
-                temperature=0.7,
-                top_p=0.3,
-                top_k=50,
-                repetition_penalty=1,
-                stop=["<｜end▁of▁sentence｜>"],
-                stream=False
-            )
-            res = response.choices[0].message.content
-
-            logger = Logger(os.path.join(self.preds_file_path, f"{i}.txt"))
-                
-            logger(q)
-            logger("=================================================================================")
-            logger(a)
-            logger("=================================================================================")
-            logger(res)
-
-            requests_sent += 1  # Increment request count
-            time.sleep(1.2)  # Ensure ~50 requests per minute
-
-            print(f"{i} ------------------------------------------\n\n\n")
-
 
 if __name__ == '__main__':
     parser=argparse.ArgumentParser()
@@ -327,6 +198,19 @@ if __name__ == '__main__':
     parser.add_argument('--shots', dest='shots', default='0')
     parser.add_argument('--save_path', dest='save_path', default='./zs_preds')
     parser.add_argument('--call_limit', dest='call_limit', default="10000")
+    parser.add_argument('--stop_point', dest='stop_point', default="-1")
+    parser.add_argument('--request_minute', dest='request_minute', default="-1")
+    parser.add_argument('--base_url', dest='base_url', default="")
+    parser.add_argument('--api_key', dest='api_key', default="OPENAI_API")
+    parser.add_argument('--client', dest='client', default="OpenAI")
+    parser.add_argument('--temperature', dest='temperature', default=None)
+    parser.add_argument('--top_p', dest='top_p', default=None)
+    parser.add_argument('--top_k', dest='top_k', default=None)
+    parser.add_argument('--max_tokens', dest='max_tokens', default=None)
+    parser.add_argument('--repetition_penalty', dest='repetition_penalty', default=None)
+    parser.add_argument('--stop_word', dest='stop_word', default=None)
+    parser.add_argument('--stream', dest='stream', default=None)
+
     args=parser.parse_args()
 
     args.rank = int(args.rank)
@@ -335,6 +219,14 @@ if __name__ == '__main__':
     args.batch_size = int(args.batch_size)
     args.shots = int(args.shots)
     args.call_limit = int(args.call_limit)
+    args.stop_point = int(args.stop_point)
+    args.request_minute = int(args.request_minute)
+    args.temperature = float(args.temperature) if args.temperature is not None else args.temperature
+    args.top_p = int(args.top_p) if args.top_p is not None else args.top_p
+    args.top_k = int(args.top_k) if args.top_k is not None else args.top_k
+    args.repetition_penalty = int(args.repetition_penalty) if args.repetition_penalty is not None else args.repetition_penalty
+    args.stream = bool(args.stream) if args.stream is not None else args.stream
+
 
     assert args.model in ["V3", "R1", "Q1.5B", "Q14B"], "Invalid model!"
     assert args.prompt_lang in ["en", "ar"], "Only 'en' and 'ar' languages supported!"
