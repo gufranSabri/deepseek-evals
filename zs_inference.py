8ca0c207873aa657a54ec3494a5abc617a52ab19
Ahmed Hasanaath <61200955+gufranSabri@users.noreply.github.com>
2025-04-19 15:15:56 +0300

    fixes

--- zs_inference.py
+++ zs_inference.py
@@ -192,7 +192,7 @@ class ZS_Inference:
 
             client = OpenAI(
                 base_url="https://api.novita.ai/v3/openai",
-                api_key="sk_XTZ7jxWmpXeqCiVG-QuFN3jj1FiDLK1EyOVudLleglk",
+                api_key="token",
             ) 
 
 
